b1bf79c7e382efb8a4fd2a694bf4ececd641c543
darwish <darwish1994@outlook.com>
2024-10-14 01:39:35 +0300

    work on set projects

--- js/projects.js
+++ js/projects.js
@@ -1,17 +1,16 @@
-$("#project_list").append( "jkdfhgjfdhjgkhdfkjk");
+$("#project_list").append("jkdfhgjfdhjgkhdfkjk");
 console.log("sdafsdfsdfdsa");
 fetch("data/data.json")
   .then((response) => response.json())
   .then((data) => {
-    $("#project_list").append(   ' <div class="col-6">');
-    $("#project_list").append(   ' <div class="row">');
-    $("#project_list").append(   ' <img src="'+data["logo"]+'" alt="app logo" class="col-2"  />');
-    $("#project_list").append(   ' <h2> <a href="'+data["app_link"]+'">'+data["title"]+'</a></h3>');
-    $("#project_list").append(   ' </div>');
+    let c =
+      ' <div class="col-6">' +
+      ' <div class="row">' +
+      ' <img src="' +data["logo"] +'" alt="app logo" class="col-2"  />' +
+      ' <h2> <a href="' + data["app_link"] +'">' +  data["title"] +"</a></h3>"+" </div>" + "<p>" + data["details"] + " </p>" + "</div>";
 
-    $("#project_list").append(   '<p>'+data["details"]+' </p>');
-
-    $("#project_list").append(   ' </div">');
+    $("#project_list").append(c);
+ 
     console.log(data);
   })
   .catch((error) => {

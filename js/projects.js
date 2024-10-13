610405ab2a4a678ce7bc246b8370ea7dde616313
darwish <darwish1994@outlook.com>
2024-10-14 01:45:07 +0300

    work on set projects

--- js/projects.js
+++ js/projects.js
@@ -3,14 +3,17 @@ console.log("sdafsdfsdfdsa");
 fetch("data/data.json")
   .then((response) => response.json())
   .then((data) => {
-    let c =
-      ' <div class="col-6">' +
-      ' <div class="row">' +
-      ' <img src="' +data.logo +'" alt="app logo" class="col-2"  />' +
-      ' <h2> <a href="' + data.app_link +'">' +  data.title +"</a></h3>"+" </div>" + "<p>" + data.details + " </p>" + "</div>";
-
+    let c = `
+    <div class="col-6">
+      <div class="row">
+        <img src="${data["logo"]}" alt="app logo" class="col-2" />
+        <h2><a href="${data["app_link"]}">${data["title"]}</a></h2>
+      </div>
+      <p>${data["details"]}</p>
+    </div>
+  `;
     $("#project_list").append(c);
- 
+
     console.log(data);
   })
   .catch((error) => {

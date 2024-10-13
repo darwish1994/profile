0c0dea440f7b3ea1d4e03cb3f9f70c77723f65b7
darwish <darwish1994@outlook.com>
2024-10-14 01:28:32 +0300

    work on set projects

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,18 @@
-fetch('data/data.json')
-  .then(response => response.json())
-  .then(data => {
+fetch("data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    $("#project_list").append(   ' <div class="col-6">');
 
-    console.log(data)
-   
+    $("#project_list").append(   ' <div class="row">');
+    $("#project_list").append(   ' <img src="'+data["logo"]+'" alt="app logo" class="col-2"  />');
+    $("#project_list").append(   ' <h2> <a href="'+data["app_link"]+'">'+data["title"]+'</a></h3>');
+    $("#project_list").append(   ' </div>');
+
+    $("#project_list").append(   '<p>'+data["details"]+' </p>');
+
+    $("#project_list").append(   ' </div">');
+    console.log(data);
   })
-  .catch(error => {
-    console.error('Error fetching JSON data:', error);
+  .catch((error) => {
+    console.error("Error fetching JSON data:", error);
   });

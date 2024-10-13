a258ab89011fe09aaee34c3a2eff11d31ec434e6
darwish <darwish1994@outlook.com>
2024-10-14 01:31:35 +0300

    work on set projects

--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,8 @@
+$("#project_list").append( "jkdfhgjfdhjgkhdfkjk"):
 fetch("data/data.json")
   .then((response) => response.json())
   .then((data) => {
     $("#project_list").append(   ' <div class="col-6">');
-
     $("#project_list").append(   ' <div class="row">');
     $("#project_list").append(   ' <img src="'+data["logo"]+'" alt="app logo" class="col-2"  />');
     $("#project_list").append(   ' <h2> <a href="'+data["app_link"]+'">'+data["title"]+'</a></h3>');
